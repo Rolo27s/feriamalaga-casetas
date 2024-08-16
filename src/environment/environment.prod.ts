8f2d43a62657ee7a058bfa1348e08f612bed9970
Rolo27s <franrodriguez2727@gmail.com>
2024-08-16 14:14:39 +0200

    gitignore updated

--- src/environment/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    googleMapsApiKey: 'AIzaSyCAQp3Chypdw7ycA_tuq8Wb3BPQ7XnfiB8'
-  };
-  
--- src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    googleMapsApiKey: 'AIzaSyCAQp3Chypdw7ycA_tuq8Wb3BPQ7XnfiB8'
-  };
-  
